25b09b0f09a21f165388a8dbc09327b3267c9ee4
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-07-01 09:28:03 +0100

    Fix 'No user' error on app initialisation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.find_or_create!(name: "Foo", email: "foo@gov.uk")
